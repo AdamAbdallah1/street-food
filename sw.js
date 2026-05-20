73b952dcf4b9223f2b59e4e2a056a5b9c8f8e22d
AdamAbdallah1 <adamabdallahayln1@gmail.com>
2026-05-20 20:20:09 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "street-food-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(["/street-food/", "/street-food/manifest.webmanifest"]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
