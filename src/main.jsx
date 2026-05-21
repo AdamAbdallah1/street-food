30941c340e1b48bbe17ee02d145422baa14f7992
AdamAbdallah1 <adamabdallahayln1@gmail.com>
2026-05-21 17:57:51 +0300

    pwa install support

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
-import "./index.css";
-
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import { Toaster } from "react-hot-toast";
+import App from './App.jsx'
 
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Toaster
       position="top-center"
@@ -16,20 +13,23 @@ createRoot(document.getElementById("root")).render(
           background: "#111",
           color: "#fff",
           borderRadius: "16px",
-          border:
-            "1px solid rgba(255,255,255,0.08)",
+          border: "1px solid rgba(255,255,255,0.08)",
         },
       }}
     />
-
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register(
-      "/demo/street-food/sw.js"
-    );
+    navigator.serviceWorker
+      .register("/demo/street-food/sw.js")
+      .then(() => {
+        console.log("SW registered");
+      })
+      .catch((err) => {
+        console.log("SW failed", err);
+      });
   });
 }
